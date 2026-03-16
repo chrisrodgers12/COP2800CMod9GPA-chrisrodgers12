33b4d8b82981e2120c545ecf68d97ccf93a9a4c7
chrisrodgers12 <s3772221@students.fscj.edu>
2026-03-15 21:29:46 -0400

    Update PalmerPenguins.java

--- edu/fscj/cop2800c/penguin/PalmerPenguins.java
+++ edu/fscj/cop2800c/penguin/PalmerPenguins.java
@@ -8,7 +8,13 @@ package edu.fscj.cop2800c.penguin;
 public class PalmerPenguins {
     public static void main(String[] args) {
         // Create an instance of PenguinAnalyzer
-
+PenguinAnalyzer analyzer = new PenguinAnalyzer();
+// Read CSV data
+analyzer.readPenguinData();
+// Print formatted output
+analyzer.printPenguinData();
+// Save results to a file
+analyzer.writePenguinData();
 
     }
 }
